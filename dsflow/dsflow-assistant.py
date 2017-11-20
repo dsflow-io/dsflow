3bcd17bb9e1970fe59b9abc46179196383f230d9
Pierre-Marie Leveque <pmleveque@users.noreply.github.com>
2017-11-20 17:47:18 +0100

    dsflow assistant: docker image new command: dsflow tree: helper to list the contents of datastore

---
+++ dsflow/dsflow-assistant.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import subprocess
+
+from dsflow_core import models
+
+
+# pwd = os.environ["PWD"]
+# tmp_abs_path = os.path.join(pwd, "tmp")
+# datastore_abs_path = os.path.join(pwd, "datastore")
+# jobs_abs_path = os.path.join(pwd, "jobs")
+#
+# DSFLOW_WORKSPACE = pwd
+#
+# docker_compose_assistant = "dsflow/docker/assistant/docker-compose.yaml"
+# # sys.argv[1]
+#
+# my_env = os.environ.copy()
+# my_env["DSFLOW_WORKSPACE"] = pwd
+#
+# args = ["docker-compose",
+#         "-f", docker_compose_assistant,
+#         ]
+#
+# # preview
+# subprocess.call(args + ["config"], env=my_env)
+#
+# # "--volume=%s:/tmp:rw" % tmp_abs_path,
+# # "--volume=%s:/data:rw" % datastore_abs_path,
+# # "--volume=%s:/jobs:ro" % jobs_abs_path,
+#
+#
+# print(" ".join(args))
+#
+# # subprocess.call(args + ["build"], env=my_env)
+# # subprocess.call(args + ["up", "-d"], env=my_env)
+#
+# subprocess.call(args + ["build"], env=my_env)
+#
+# subprocess.call(args + ["run", "--service-ports", "assistant"], env=my_env)
---
+++ dsflow/dsflow-tree.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import subprocess
+
+from dsflow_core import models
+
+pwd = os.environ["PWD"]
+tmp_abs_path = os.path.join(pwd, "tmp")
+datastore_abs_path = os.path.join(pwd, "datastore")
+jobs_abs_path = os.path.join(pwd, "jobs")
+
+DSFLOW_WORKSPACE = pwd
+
+docker_compose_assistant = "dsflow/docker/assistant/docker-compose.yaml"
+
+directory = sys.argv[1] if len(sys.argv) > 1 else "/data"
+depth = sys.argv[2] if len(sys.argv) > 2 else "2"
+
+my_env = os.environ.copy()
+my_env["DSFLOW_WORKSPACE"] = pwd
+
+args = ["docker-compose",
+        "-f", docker_compose_assistant,
+        ]
+
+subprocess.call(args + ["run", "assistant", "tree", directory, "-dCL", depth], env=my_env)
