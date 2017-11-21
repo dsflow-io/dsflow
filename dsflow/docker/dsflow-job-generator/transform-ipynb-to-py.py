b7fc82edded2337f7cf64e9e1f4e40b1b7b076fe
Pierre-Marie Leveque <pmleveque@users.noreply.github.com>
2017-11-21 21:55:51 +0100

    new dsflow command: `dsflow transform-notebook` this will transform a notebook into a python script

---
+++ dsflow/docker/dsflow-job-generator/transform-ipynb-to-py.py
@@ -0,0 +1,41 @@
+import os
+import sys
+from dsflow_core.utils import *
+from dsflow_core.models import *
+
+DSFLOW_ROOT = os.environ["DSFLOW_ROOT"]
+
+
+def transform(notebook_name):
+    """Convert notebook NOTEBOOK_NAME into a task.
+    WARNING!
+    Currently, it only converts the notebook into a Python file,
+    but does not create the task.
+    """
+
+    input_file = '/jobs/%s.ipynb' % notebook_name
+    output_file = '/jobs/%s.py' % notebook_name
+    click.echo("will create %s" % output_file)
+
+    mkdir_if_needed(filename=output_file)
+
+    # if os.path.isfile(output_file):
+    #     raise Exception("File already exists. You can edit it using `dsflow notebooks`")
+
+    with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
+        nb = json.load(infile)
+        if nb["nbformat"] >= 4:
+            for i, cell in enumerate(nb["cells"]):
+                # outfile.write("#cell "+str(i)+"\n")
+                if cell["cell_type"] == "code":
+                    outfile.write("# <codecell>\n\n")
+                    for line in cell["source"]:
+                        outfile.write(line)
+                else:
+                    outfile.write("# <markdowncell>\n\n")
+                    for line in cell["source"]:
+                        outfile.write("# " + line)
+                outfile.write('\n\n')
+
+
+transform(sys.argv[1])
---
+++ dsflow/dsflow-transform-notebook.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import subprocess
+
+from dsflow_core.cli_utils import validate_env
+
+validate_env()
+
+# Input parameters
+input_parameters = sys.argv[1:]
+
+# Paths, ids and env. variables
+DSFLOW_WORKSPACE = os.environ["DSFLOW_WORKSPACE"]
+datastore_abs_path = os.path.join(DSFLOW_WORKSPACE, "datastore")
+jobs_abs_path = os.path.join(DSFLOW_WORKSPACE, "jobs")
+
+docker_image_dir = "dsflow-job-generator"
+docker_image_id = "dsflow/%s" % docker_image_dir
+
+image_id = "dsflow-job-generator"
+docker_compose_file = "dsflow/docker/%s/docker-compose.yaml" % image_id
+
+# Rebuild docker image
+if input_parameters and input_parameters[0] == "--build":
+    subprocess.call([
+        "docker-compose",
+        "-f", docker_compose_file,
+        "build"])
+
+    input_parameters = input_parameters[1:]
+
+args = [
+    "docker-compose",
+    "-f", docker_compose_file,
+    "run",
+    "job-generator",
+    "python",
+    "transform-ipynb-to-py.py",
+] + input_parameters
+
+print("\n========== preview docker run command ===========")
+print(" ".join(args))
+
+print("\n========== running docker container ===========")
+subprocess.call(args)
