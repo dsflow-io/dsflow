52be822c8699e2ae01870931037e0987a5351176
Pierre-Marie Leveque <pmleveque@users.noreply.github.com>
2017-11-23 15:53:45 +0100

    New `dsflow compose` command This is a shortcut for docker-compose used with dsflow project configuration.

---
+++ dsflow/dsflow-compose.py
@@ -0,0 +1,28 @@
+import sys
+import os
+import subprocess
+
+from python_scripts.dsflow_core.cli_utils import validate_env
+
+validate_env()
+
+# Input parameters
+dsflow_service_name = sys.argv[1]
+docker_compose_file = DSFLOW_ROOT + "/docker/%s/docker-compose.yaml" % dsflow_service_name
+
+input_parameters = sys.argv[2:]
+
+my_env = os.environ.copy()
+
+args = ["docker-compose",
+        "-f", docker_compose_file,
+        ]
+
+# preview
+subprocess.call(args + ["config"], env=my_env)
+
+print(" ".join(args))
+
+subprocess.call(args + ["up", "--build", "-d"], env=my_env)
+
+subprocess.call(args + ["exec", dsflow_service_name, "/bin/bash"], env=my_env)
