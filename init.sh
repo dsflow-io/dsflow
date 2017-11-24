140565d093f8c3392cf89225b73a770b153501f2
Pierre-Marie Leveque <pmleveque@users.noreply.github.com>
2017-11-24 18:42:40 +0100

    create docker network in init.sh

--- init.sh
+++ init.sh
@@ -7,12 +7,17 @@ else
     echo "pip not found. Please install Python."
 fi
 
+# create dsflow docker network
+docker network create dsflow
 
+# exporting env. variables
 export DSFLOW_WORKSPACE=$(pwd)
 export DSFLOW_ROOT=$(pwd)/dsflow
 
+# exporting dsflow
 alias dsflow='function _dsflow(){ if [[ -z "$1" ]]; then python $DSFLOW_ROOT/dsflow-menu.py; else python $DSFLOW_ROOT/dsflow-$1.py "${@:2}"; fi; };_dsflow'
 
+# prompt user about docker images
 read -p "Would you like to download and build docker images? [y/N]" -n 1 -r
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
