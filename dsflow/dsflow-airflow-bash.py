9a9f58db0ac811d574ed77cb5d3392e0a3bc45ee
Pierre-Marie Leveque <pmleveque@users.noreply.github.com>
2017-11-30 18:35:14 +0100

    Refers to #6: configure Airflow container so it can run dsflow jobs. Note: this work is not completely done, because the airflow container runs with user root, which is not the proper way to do it.

--- dsflow/dsflow-airflow-bash.py
+++ dsflow/dsflow-airflow-bash.py
@@ -6,9 +6,11 @@ from python_scripts.dsflow_core.cli_utils import validate_env
 
 validate_env()
 
-DSFLOW_ROOT = os.environ["DSFLOW_ROOT"]
 DSFLOW_WORKSPACE = os.environ["DSFLOW_WORKSPACE"]
+DSFLOW_ROOT = os.environ["DSFLOW_ROOT"]
 
+# Input parameters
+dsflow_service_name = "airflow"
 docker_compose_base_file = DSFLOW_ROOT + "/docker/base/docker-compose.yaml"
 docker_compose_db = DSFLOW_ROOT + "/docker/db/docker-compose.yaml"
 docker_compose_airflow = DSFLOW_ROOT + "/docker/airflow/docker-compose.yaml"
@@ -22,10 +24,7 @@ args = ["docker-compose",
         "-f", docker_compose_airflow,
         ]
 
-print("\n========== calling docker-compose ====================")
+# preview
 print(" ".join(args))
-subprocess.call(args + ["up", "-d"], env=my_env)
 
-print("\nAirflow web interface is accessible at http://localhost:8081/")
-print("Hint: `dsflow airflow-bash` will give you access to "
-      "airflow container bash")
+subprocess.call(args + ["exec", dsflow_service_name, "/bin/bash"], env=my_env)
